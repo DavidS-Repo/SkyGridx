167df4e2d4c37643da65d143c70428946ff1ec8a
David Serrano <serranodavid983@gmail.com>
2023-12-24 21:00:43 -0800

    Update OreGen.java
    Refactored OreGen class to dynamically generate stone and cobblestone ores based on YAML configuration. Implemented distinct handling for STONE and COBBLESTONE block generation through water and lava sources.

--- SkyGridPlugin/src/main/OreGen.java
+++ SkyGridPlugin/src/main/OreGen.java
@@ -7,87 +7,93 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 public class OreGen implements Listener {
-	private final JavaPlugin plugin;
-	private final Random random;
-	private final Map<Material, Double> oreChances;
+    private final JavaPlugin plugin;
+    private final Map<Material, Double> stoneChances;
+    private final Map<Material, Double> cobblestoneChances;
 
-	public OreGen(JavaPlugin plugin) {
-		this.plugin = plugin;
-		this.random = new Random();
-		this.oreChances = new HashMap<>();
-		loadOreChances();
-	}
+    public OreGen(JavaPlugin plugin) {
+        this.plugin = plugin;
+        this.stoneChances = new HashMap<>();
+        this.cobblestoneChances = new HashMap<>();
+        loadOreChances();
+    }
 
-	// Method to load ore chances from the "ores.txt" file
-	private void loadOreChances() {
-		File file = new File(plugin.getDataFolder(), "OreGenBlock/ores.txt");
-		if (file.exists()) {
-			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-				String line;
-				while ((line = reader.readLine()) != null) {
-					line = line.trim();
-					if (line.isEmpty() || line.startsWith("#")) {
-						// Skip empty lines or lines starting with '#'
-						continue;
-					}
-					String[] parts = line.split(":");
-					if (parts.length == 2) {
-						Material oreMaterial = Material.getMaterial(parts[0]);
-						double chance = Double.parseDouble(parts[1]) / 100.0;
-						if (oreMaterial != null && chance > 0 && chance <= 1) {
-							oreChances.put(oreMaterial, chance);
-						} else {
-							plugin.getLogger().warning("Invalid ore data in ores.txt: " + line);
-						}
-					} else {
-						plugin.getLogger().warning("Invalid line in ores.txt: " + line);
-					}
-				}
-			} catch (IOException | NumberFormatException e) {
-				e.printStackTrace();
-			}
-		} else {
-			plugin.getLogger().warning("Missing ores.txt file. No ores will be generated.");
-		}
-	}
+    private void loadOreChances() {
+        File configFile = new File(plugin.getDataFolder(), "OreGenBlock/ores.yml");
+        if (configFile.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(configFile))) {
+                String line;
+                String currentBlockType = "";
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (line.isEmpty() || line.startsWith("#")) {
+                        continue;
+                    }
+                    String[] parts = line.split(":");
+                    if (parts.length == 1) {
+                        currentBlockType = parts[0].trim();
+                    } else if (parts.length == 2) {
+                        Material oreMaterial = Material.getMaterial(parts[0].trim());
+                        double chance = Double.parseDouble(parts[1].trim()) / 100.0;
+                        if (oreMaterial != null && chance > 0 && chance <= 1) {
+                            if ("STONE".equalsIgnoreCase(currentBlockType)) {
+                                stoneChances.put(oreMaterial, chance);
+                            } else if ("COBBLESTONE".equalsIgnoreCase(currentBlockType)) {
+                                cobblestoneChances.put(oreMaterial, chance);
+                            }
+                        } else {
+                            plugin.getLogger().warning("Invalid ore data in ores.yml: " + line);
+                        }
+                    } else {
+                        plugin.getLogger().warning("Invalid line in ores.yml: " + line);
+                    }
+                }
+            } catch (IOException | NumberFormatException e) {
+                e.printStackTrace();
+            }
+        } else {
+            plugin.getLogger().warning("Missing ores.yml file. No ores will be generated.");
+        }
+    }
 
-	@EventHandler
-	public void onBlockForm(BlockFormEvent event) {
-		Block block = event.getBlock();
-		Material toType = event.getNewState().getType();
-		if (toType == Material.STONE) {
-			Material fromType = block.getType();
-			if (fromType == Material.LAVA || fromType == Material.WATER) {
-				Material oreMaterial = getRandomOreMaterial();
-				if (oreMaterial != null) {
-					event.setCancelled(true); // Cancel the block form event
-					block.setType(oreMaterial, true); // Set the chosen ore block at the same location
-				}
-			}
-		}
-	}
+    @EventHandler
+    public void onBlockForm(BlockFormEvent event) {
+        Block block = event.getBlock();
+        Material toType = event.getNewState().getType();
+        Material fromType = block.getType();
+        if ((toType == Material.STONE || toType == Material.COBBLESTONE) &&
+                (fromType == Material.LAVA || fromType == Material.WATER)) {
+            Material replacement = determineReplacement(toType);
+            if (replacement != null) {
+                event.setCancelled(true);
+                block.setType(replacement, true);
+            }
+        }
+    }
 
-	private Material getRandomOreMaterial() {
-		double totalChance = 0;
-		for (double chance : oreChances.values()) {
-			totalChance += chance;
-		}
-		double randomValue = random.nextDouble() * totalChance;
-		for (Map.Entry<Material, Double> entry : oreChances.entrySet()) {
-			randomValue -= entry.getValue();
-			if (randomValue <= 0) {
-				return entry.getKey();
-			}
-		}
-		return null;
-	}
+    private Material determineReplacement(Material blockType) {
+        if (blockType == Material.STONE) {
+            return getRandomOreMaterial(stoneChances);
+        } else if (blockType == Material.COBBLESTONE) {
+            return getRandomOreMaterial(cobblestoneChances);
+        }
+        return null;
+    }
+
+    private Material getRandomOreMaterial(Map<Material, Double> oreChances) {
+        double totalChance = oreChances.values().stream().mapToDouble(Double::doubleValue).sum();
+        double randomValue = Math.random() * totalChance;
+        for (Map.Entry<Material, Double> entry : oreChances.entrySet()) {
+            randomValue -= entry.getValue();
+            if (randomValue <= 0) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
 }
