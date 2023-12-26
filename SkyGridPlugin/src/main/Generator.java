6a3069456c4b85ce24acb3699adaad341bfc053f
David Serrano <serranodavid983@gmail.com>
2023-12-26 09:33:26 -0800

    Update Generator.java
    Rework from the ground up to move away from relying on files for generation, now more optimized using chunk events for generation.

--- SkyGridPlugin/src/main/Generator.java
+++ SkyGridPlugin/src/main/Generator.java
@@ -5,15 +5,16 @@ import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.type.Bamboo;
+import org.bukkit.block.data.type.CaveVines;
+import org.bukkit.block.data.type.Leaves;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.block.data.Ageable;
-import org.bukkit.block.data.type.Leaves;
-import org.bukkit.block.data.type.Bamboo;
-import org.bukkit.block.data.type.CaveVines;
-import org.bukkit.block.data.BlockData;
 
 import java.io.*;
 import java.util.*;
@@ -23,37 +24,25 @@ public class Generator implements Listener {
 	private final Random random;
 	private final Map<String, List<Material>> worldMaterials;
 	private final Map<String, Set<String>> generatedChunksByWorld;
-	private final Map<String, Map<Material, Integer>> biomeMaterials; // Biome-specific block data
-	private final Spawner spawner; // Create a field to hold the Spawner instance
-	private final Chest chest; // Create a field to hold the Spawner instance
-
-	// Variable to set the number of chunks in which the blocks are distributed
+	private final Map<String, Map<Material, Integer>> biomeMaterials;
+	private final Spawner spawner;
+	private final Chest chest;
+	private final int PROCESS_DELAY = 10;
 	private final double chunksForDistribution = 64;
+	private boolean firstBootComplete = false;
 
-	// Delay in ticks before processing the chunk after it is loaded
-	private final int PROCESS_DELAY = 12;
-
-	// Variable to track the number of chunks being processed for each dimension
-	private final Map<World.Environment, Integer> chunksProcessedByDimension;
-
-	public Generator(JavaPlugin plugin, boolean clearChunksBeforeGeneration) {
+	public Generator(JavaPlugin plugin) {
 		this.plugin = plugin;
 		this.random = new Random();
 		this.worldMaterials = new HashMap<>();
 		this.generatedChunksByWorld = new HashMap<>();
 		this.biomeMaterials = new HashMap<>();
-		this.chunksProcessedByDimension = new HashMap<>();
-		// Initialize the Spawner instance
 		this.spawner = Spawner.getInstance(plugin);
-		// Initialize the Spawner instance
 		this.chest = Chest.getInstance(plugin);
 	}
 
-	// Method to initialize the generator and load data from files
 	public void initialize() {
 		loadWorldMaterials();
-		loadGeneratedChunks();
-		processLoadedChunks();
 		registerEvents();
 		callOreGen();
 	}
@@ -63,7 +52,6 @@ public class Generator implements Listener {
 		plugin.getServer().getPluginManager().registerEvents(oreGen, plugin);
 	}
 
-	// Method to load world materials from text files
 	private void loadWorldMaterials() {
 		loadMaterialsForWorld("world.txt");
 		loadMaterialsForWorld("world_nether.txt");
@@ -73,10 +61,11 @@ public class Generator implements Listener {
 	private void loadMaterialsForWorld(String fileName) {
 		plugin.getLogger().info("Loading materials for world from file: " + fileName);
 		File file = new File(plugin.getDataFolder(), "SkygridBlocks/" + fileName);
+
 		if (file.exists()) {
 			List<Material> materials = new ArrayList<>();
-			List<Material> materialsWithoutPercentage = new ArrayList<>(); // New list for items without percentages
-			double remainingPercentage = 100.0; // Initialize remaining percentage
+			List<Material> materialsWithoutPercentage = new ArrayList<>();
+			double remainingPercentage = 100.0;
 
 			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 				String line;
@@ -84,14 +73,12 @@ public class Generator implements Listener {
 
 				while ((line = reader.readLine()) != null) {
 					if (line.trim().startsWith("#")) {
-						continue; // Skip comments
+						continue;
 					}
-
 					if (line.trim().startsWith("-")) {
 						currentBiome = line.trim().replace("-", "");
-						continue; // Start of a new biome section
+						continue;
 					}
-
 					String[] parts = line.split(":");
 					Material material = Material.getMaterial(parts[0]);
 
@@ -99,11 +86,9 @@ public class Generator implements Listener {
 						int count = (int) Math.ceil(parsePercentage(parts[1]) * chunksForDistribution);
 
 						if (currentBiome != null) {
-							// Add biome-specific block data
 							Map<Material, Integer> biomeMaterialsMap = biomeMaterials.computeIfAbsent(currentBiome, k -> new HashMap<>());
 							biomeMaterialsMap.put(material, count);
 						} else {
-							// If no biome-specific data, add to global materials
 							for (int i = 0; i < count; i++) {
 								materials.add(material);
 							}
@@ -111,8 +96,6 @@ public class Generator implements Listener {
 						}
 					}
 				}
-
-				// Calculate the number of items to distribute the remaining percentage
 				int remainingItemCount = materialsWithoutPercentage.size();
 				if (remainingItemCount > 0 && remainingPercentage > 0) {
 					double percentagePerItem = remainingPercentage / remainingItemCount;
@@ -123,7 +106,6 @@ public class Generator implements Listener {
 						}
 					}
 				}
-
 				if (!materials.isEmpty()) {
 					String worldName = getWorldNameFromFileName(fileName);
 					plugin.getLogger().info("Materials loaded for world: " + worldName);
@@ -142,7 +124,7 @@ public class Generator implements Listener {
 			return Double.parseDouble(percentageString);
 		} catch (NumberFormatException e) {
 			plugin.getLogger().warning("Invalid percentage format: " + percentageString);
-			return 1.0; // Default to 1% if invalid format
+			return 1.0;
 		}
 	}
 
@@ -154,49 +136,37 @@ public class Generator implements Listener {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 
-	private void loadGeneratedChunks() {
-		// Load generated chunks for each world type
-		for (String worldName : worldMaterials.keySet()) {
-			File file = new File(plugin.getDataFolder(), worldName + "_generated_chunks.txt");
-			Set<String> chunks = new HashSet<>();
-			if (file.exists()) {
-				try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-					String line;
-					while ((line = reader.readLine()) != null) {
-						chunks.add(line);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			generatedChunksByWorld.put(worldName, chunks);
-		}
-	}
-
-	private void saveGeneratedChunks() {
-		// Save generated chunks for each world type
-		for (String worldName : generatedChunksByWorld.keySet()) {
-			File file = new File(plugin.getDataFolder(), worldName + "_generated_chunks.txt");
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-				for (String chunkCoords : generatedChunksByWorld.get(worldName)) {
-					writer.write(chunkCoords);
-					writer.newLine();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onChunkLoad(ChunkLoadEvent event) {
-		Chunk chunk = event.getChunk();
-		String chunkCoords = chunk.getX() + "," + chunk.getZ();
-		String worldName = chunk.getWorld().getName();
-		Set<String> generatedChunks = generatedChunksByWorld.getOrDefault(worldName, new HashSet<>());
+		if (!firstBootComplete && plugin instanceof SkyGridPlugin && ((SkyGridPlugin) plugin).isFirstBoot()) {
+			processAllLoadedChunks();
+			firstBootComplete = true;
+			plugin.getLogger().info("First boot generation complete.");
+		} else {
+			Chunk chunk = event.getChunk();
+			String chunkCoords = chunk.getX() + "," + chunk.getZ();
+			String worldName = chunk.getWorld().getName();
+			Set<String> generatedChunks = generatedChunksByWorld.getOrDefault(worldName, new HashSet<>());
 
-		if (!generatedChunks.contains(chunkCoords)) {
-			Bukkit.getScheduler().runTaskLater(plugin, () -> processChunk(chunk), PROCESS_DELAY);
+			if (event.isNewChunk() && !generatedChunks.contains(chunkCoords)) {
+				Bukkit.getScheduler().runTaskLater(plugin, () -> processChunk(chunk), PROCESS_DELAY);
+			} else {
+				generatedChunks.add(chunkCoords);
+			}
+		}
+	}
+
+	private void processAllLoadedChunks() {
+		for (World world : Bukkit.getWorlds()) {
+			for (Chunk loadedChunk : world.getLoadedChunks()) {
+				String chunkCoords = loadedChunk.getX() + "," + loadedChunk.getZ();
+				String worldName = loadedChunk.getWorld().getName();
+				Set<String> generatedChunks = generatedChunksByWorld.getOrDefault(worldName, new HashSet<>());
+
+				if (!generatedChunks.contains(chunkCoords)) {
+					processChunk(loadedChunk);
+				}
+			}
 		}
 	}
 
@@ -211,7 +181,6 @@ public class Generator implements Listener {
 		if (dimension == null) {
 			return;
 		}
-
 		List<Chunk> chunksToProcess = new ArrayList<>();
 		for (World world : Bukkit.getWorlds()) {
 			if (world.getEnvironment() == dimension) {
@@ -226,10 +195,8 @@ public class Generator implements Listener {
 				}
 			}
 		}
-
 		int totalChunks = chunksToProcess.size();
 		int batchSize = Math.max(1, (totalChunks + 1) / 2);
-
 		Bukkit.getScheduler().runTaskLater(plugin, () -> processChunksBatch(dimension, chunksToProcess, batchSize, 0, dimensionQueue), PROCESS_DELAY);
 	}
 
@@ -242,7 +209,6 @@ public class Generator implements Listener {
 			Chunk chunk = chunksToProcess.get(i);
 			processChunk(chunk);
 		}
-
 		if (endIdx < totalChunks) {
 			Bukkit.getScheduler().runTaskLater(plugin, () -> processChunksBatch(dimension, chunksToProcess, batchSize, batchIndex + 1, dimensionQueue), PROCESS_DELAY);
 		} else {
@@ -262,18 +228,9 @@ public class Generator implements Listener {
 		String biomeName = getBiomeName(chunk);
 
 		switch (dimension) {
-		case NETHER:
-			minY = 0;
-			maxY = 128;
-			break;
-		case THE_END:
-			minY = 0;
-			maxY = 128;
-			break;
-		default: // Overworld
-			minY = -64;
-			maxY = 64;
-			break;
+		case NETHER: minY = 0; maxY = 128; break;
+		case THE_END: minY = 0; maxY = 128; break;
+		default: minY = -64; maxY = 64; break;
 		}
 
 		for (int x = xStart + 1; x <= xStart + 13; x += 4) {
@@ -306,7 +263,12 @@ public class Generator implements Listener {
 								Bamboo bamboo = (Bamboo) blockData;
 								bamboo.setStage(1);
 								block.setBlockData(bamboo, false);
-							} else if (material == Material.CACTUS || material == Material.SUGAR_CANE || material == Material.KELP || material == Material.WHEAT || material == Material.BEETROOTS || material == Material.CARROTS || material == Material.POTATOES || material == Material.TORCHFLOWER_CROP || material == Material.PITCHER_CROP || material == Material.NETHER_WART || material == Material.CAVE_VINES) {
+							} else if (material == Material.CACTUS || material == Material.SUGAR_CANE 
+									|| material == Material.KELP || material == Material.WHEAT 
+									|| material == Material.BEETROOTS || material == Material.CARROTS 
+									|| material == Material.POTATOES || material == Material.TORCHFLOWER_CROP 
+									|| material == Material.PITCHER_CROP || material == Material.NETHER_WART 
+									|| material == Material.CAVE_VINES) {
 								if (blockData instanceof Ageable) {
 									Ageable ageable = (Ageable) blockData;
 									int maxAge = ageable.getMaximumAge();
@@ -320,7 +282,7 @@ public class Generator implements Listener {
 								}
 							} else if (material == Material.SPAWNER) {
 								spawner.BlockInfo(block);
-							}else if (material == Material.CHEST) {
+							} else if (material == Material.CHEST) {
 								chest.loadChest(block);
 							}
 						}
@@ -331,20 +293,8 @@ public class Generator implements Listener {
 				}
 			}
 		}
-
-		int remainingChunks = chunksProcessedByDimension.getOrDefault(dimension, 0) - 1;
-		chunksProcessedByDimension.put(dimension, remainingChunks);
-
-		if (remainingChunks == 0) {
-			plugin.getLogger().info("Chunk generation completed for " + dimension.toString().toLowerCase().replace("_", " ") + "!");
-		}
-
-		String chunkCoords = chunk.getX() + "," + chunk.getZ();
-		generatedChunksByWorld.get(worldName).add(chunkCoords);
-		saveGeneratedChunks();
 	}
 
-	// Method to get a random material for a specific world and biome, respecting percentages
 	private Material getRandomMaterialForWorld(String worldName, String biomeName) {
 		List<Material> materials = worldMaterials.get(worldName);
 		Map<Material, Integer> biomeMaterialsMap = biomeMaterials.get(biomeName);
@@ -353,7 +303,6 @@ public class Generator implements Listener {
 			if (biomeMaterialsMap != null && !biomeMaterialsMap.isEmpty()) {
 				List<Material> possibleMaterials = new ArrayList<>();
 
-				// Add biome-specific materials
 				for (Map.Entry<Material, Integer> entry : biomeMaterialsMap.entrySet()) {
 					Material material = entry.getKey();
 					int count = entry.getValue();
@@ -361,21 +310,16 @@ public class Generator implements Listener {
 						possibleMaterials.add(material);
 					}
 				}
-
 				if (!possibleMaterials.isEmpty()) {
 					int randomIndex = random.nextInt(possibleMaterials.size());
 					return possibleMaterials.get(randomIndex);
 				}
 			}
-
-			// If there are no biome-specific materials, use the global materials list
 			int randomIndex = random.nextInt(materials.size());
 			return materials.get(randomIndex);
 		}
-
 		return null;
 	}
-
 	private String getBiomeName(Chunk chunk) {
 		return chunk.getBlock(7, 64, 7).getBiome().name();
 	}
