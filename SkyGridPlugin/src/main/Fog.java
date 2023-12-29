8322051c90a42c8b3f81a97da6d22c88c8087248
David Serrano <serranodavid983@gmail.com>
2023-12-29 15:56:13 -0800

    Update Fog.java
    - Added a constructor to accept a ResourcePackManager object
    - Removed static declarations for fogBossBar and fogPlayers, and made them instance variables
    - Initialized fogBossBar visibility in the constructor
    - Updated onCommand method to use a switch case instead of if-else statements
    - Updated applyFog method to clear the fogBossBar and fogPlayers before adding the admin player
    - Updated stopFogTask method to cancel the fogTask and clear the fogBossBar and fogPlayers
    - Updated clearFog method to clear the fogBossBar and fogPlayers, and add the admin player if not null

--- SkyGridPlugin/src/main/Fog.java
+++ SkyGridPlugin/src/main/Fog.java
@@ -18,85 +18,81 @@ import java.util.Set;
 
 public class Fog implements CommandExecutor {
 
-    private static final BossBar fogBossBar = Bukkit.createBossBar(ChatColor.WHITE + "", BarColor.WHITE, BarStyle.SOLID, BarFlag.CREATE_FOG);
-    private static final Set<Player> fogPlayers = new HashSet<>();
+	private static final BossBar fogBossBar = Bukkit.createBossBar(ChatColor.WHITE + "", BarColor.WHITE, BarStyle.SOLID, BarFlag.CREATE_FOG);
+	private static final Set<Player> fogPlayers = new HashSet<>();
+	private final ResourcePackManager manager;
+	private BukkitRunnable fogTask;
 
-    static {
-        fogBossBar.setVisible(true);
-    }
+	public Fog(ResourcePackManager manager) {
+		this.manager = manager;
+		fogBossBar.setVisible(true);
+	}
 
-    private static BukkitRunnable fogTask;
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (!(sender instanceof Player) && !sender.isOp()) {
+			sender.sendMessage(ChatColor.RED + "Only OPs can use this command in the console.");
+			return true;
+		}
+		Player player = (sender instanceof Player) ? (Player) sender : null;
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player) && !sender.isOp()) {
-            sender.sendMessage(ChatColor.RED + "Only OPs can use this command in the console.");
-            return true;
-        }
+		if (player != null && !player.isOp()) {
+			player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
+			return true;
+		}
+		switch (label.toLowerCase()) {
+		case "fogon":
+			applyFog(player);
+			startFogTask();
+			sender.sendMessage(ChatColor.GREEN + "Fog enabled for Overworld and End.");
+			manager.setEnabled(true);
+			break;
+		case "fogoff":
+			clearFog(player);
+			stopFogTask();
+			sender.sendMessage(ChatColor.RED + "Fog disabled for Overworld and End.");
+			manager.setEnabled(false);
+			break;
+		}
+		return true;
+	}
 
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
+	private void applyFog(Player admin) {
+		fogBossBar.removeAll();
+		fogPlayers.clear();
+		if (admin != null) {
+			fogBossBar.addPlayer(admin);
+			fogPlayers.add(admin);
+		}
+	}
 
-            // Check if the player has OP permission
-            if (!player.isOp()) {
-                player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
-                return true;
-            }
+	private void startFogTask() {
+		fogTask = new BukkitRunnable() {
+			public void run() {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					fogBossBar.addPlayer(player);
+					fogPlayers.add(player);
+				}
+			}
+		};
+		fogTask.runTaskTimer(JavaPlugin.getPlugin(SkyGridPlugin.class), 0, 20);
+	}
 
-            if (label.equalsIgnoreCase("fogon")) {
-                applyFog(player);
-                startFogTask();
-                player.sendMessage(ChatColor.GREEN + "Fog enabled for Overworld and End.");
-            } else if (label.equalsIgnoreCase("fogoff")) {
-                clearFog(player);
-                stopFogTask();
-                player.sendMessage(ChatColor.RED + "Fog disabled for Overworld and End.");
-            }
-        } else {
-            // For console execution
-            if (label.equalsIgnoreCase("fogon")) {
-                applyFog(null); // Pass null as there's no player associated with console
-                startFogTask();
-                sender.sendMessage(ChatColor.GREEN + "Fog enabled for Overworld and End.");
-            } else if (label.equalsIgnoreCase("fogoff")) {
-                clearFog(null); // Pass null as there's no player associated with console
-                stopFogTask();
-                sender.sendMessage(ChatColor.RED + "Fog disabled for Overworld and End.");
-            }
-        }
-        return true;
-    }
+	private void stopFogTask() {
+		if (fogTask != null) {
+			fogTask.cancel();
+			fogTask = null;
+		}
+		fogBossBar.removeAll();
+		fogPlayers.clear();
+	}
 
-    private void applyFog(Player admin) {
-        fogBossBar.removeAll();
-        fogPlayers.clear();
-
-        if (admin != null) {
-            fogBossBar.addPlayer(admin);
-            fogPlayers.add(admin);
-        }
-    }
-
-    private void startFogTask() {
-        fogTask = new BukkitRunnable() {
-            public void run() {
-                for (Player player : Bukkit.getOnlinePlayers()) {
-                    fogBossBar.addPlayer(player);
-                    fogPlayers.add(player);
-                }
-            }
-        };
-        fogTask.runTaskTimer(JavaPlugin.getPlugin(SkyGridPlugin.class), 0, 20);
-    }
-
-    private void stopFogTask() {
-        if (fogTask != null) {
-            fogTask.cancel();
-        }
-    }
-
-    private void clearFog(Player admin) {
-        fogBossBar.removeAll();
-        fogPlayers.clear();
-    }
+	private void clearFog(Player admin) {
+		fogBossBar.removeAll();
+		fogPlayers.clear();
+		if (admin != null) {
+			fogBossBar.addPlayer(admin);
+			fogPlayers.add(admin);
+		}
+	}
 }
