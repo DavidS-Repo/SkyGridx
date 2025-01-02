87b9439e329e37f8f4f7a39e9de15b300d5e1075
David S. <serranodavid983@gmail.com>
2025-01-02 11:21:40 -0800

    paper_url updated to version 72

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@
 :: Set variables
 set plugin_url=https://raw.githubusercontent.com/DavidS-Repo/SkyGridx/main/SkyGrid.jar
 set datapack_url=https://raw.githubusercontent.com/DavidS-Repo/SkyGridx/main/DataPacks/Void-Biomes-1.21.04.zip
-set paper_url=https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/70/downloads/paper-1.21.4-70.jar
+set paper_url=https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/72/downloads/paper-1.21.4-72.jar
 
 set plugin_folder=plugins
 set world_folder=world
