d306f55aa88f39fea41e5aa1a0c55343ff19f358
David S. <serranodavid983@gmail.com>
2025-04-30 16:30:39 -0700

    Fix: /miniregen remove
    Previously, using "remove chunk" didn't work because the command handler never checked for the "chunk" keyword and treated it as the alias instead.
    
    Now the command properly reads "chunk" as an argument and deletes the actual alias provided after it. This also makes the command consistent with tab completion, which already supported "remove chunk".

--- SkyGridPlugin/main/MiniRegenCommand.java
+++ SkyGridPlugin/main/MiniRegenCommand.java
@@ -20,56 +20,69 @@ public class MiniRegenCommand implements CommandExecutor {
 			return true;
 		}
 		Player player = (Player) sender;
-		World world = player.getWorld();
-		int currentChunkX = player.getLocation().getChunk().getX();
-		int currentChunkZ = player.getLocation().getChunk().getZ();
+		World world   = player.getWorld();
+		int chunkX    = player.getLocation().getChunk().getX();
+		int chunkZ    = player.getLocation().getChunk().getZ();
 
 		if (args.length == 0) {
-			Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen <add|remove> [interval] [alias] [distribution] [group] OR /miniregen remove [alias] OR /miniregen remove group [groupName]");
+			Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen <add|remove> ...");
 			return true;
 		}
 
 		if (args[0].equalsIgnoreCase("add")) {
 			if (args.length < 4) {
-				Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen add [interval in seconds] [alias] [distribution] [group(optional)]");
+				Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen add <intervalSec> <alias> <distribution> [group]");
 				return true;
 			}
-			int interval = 600;
+			int interval;
 			try {
 				interval = Integer.parseInt(args[1]);
-			} catch (NumberFormatException e) {
-				Cc.sendS(player, Cc.RED, "Invalid interval. Using default 600 seconds (10 Minutes).");
+			} catch (NumberFormatException ex) {
+				interval = 600;
+				Cc.sendS(player, Cc.RED, "Invalid interval, using 600 s.");
 			}
+
 			String alias = args[2];
 			String distribution = args[3];
-			String group = "";
-			if (args.length >= 5) {
-				group = args[4];
-			}
-			miniRegenManager.addMiniRegen(world.getName(), currentChunkX, currentChunkZ, alias, distribution, interval, group);
-			Cc.sendS(player, Cc.GREEN, "This chunk has been added for mini regeneration with alias '" + alias + "', interval " 
-					+ interval + " seconds, distribution '" + distribution + "'" + (group.isEmpty() ? "" : " and group '" + group + "'") + ".");
-		} else if (args[0].equalsIgnoreCase("remove")) {
+			String group = args.length >= 5 ? args[4] : "";
+
+			miniRegenManager.addMiniRegen(world.getName(), chunkX, chunkZ, alias, distribution, interval, group);
+
+			Cc.sendS(player, Cc.GREEN, "Chunk added with alias '" + alias + "'.");
+			return true;
+		}
+
+		if (args[0].equalsIgnoreCase("remove")) {
 			if (args.length < 2) {
-				Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen remove [alias] OR /miniregen remove group [groupName]");
+				Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen remove <alias> | /miniregen remove chunk <alias> | /miniregen remove group <group>");
 				return true;
 			}
 			if (args[1].equalsIgnoreCase("group")) {
 				if (args.length < 3) {
-					Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen remove group [groupName]");
+					Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen remove group <group>");
 					return true;
 				}
 				String groupName = args[2];
 				miniRegenManager.removeMiniRegenGroup(groupName);
-				Cc.sendS(player, Cc.GREEN, "All mini regeneration settings in group '" + groupName + "' have been removed.");
-			} else {
-				String alias = args[1];
-				miniRegenManager.removeMiniRegen(alias);
-				Cc.sendS(player, Cc.GREEN, "The mini regeneration setting with alias '" + alias + "' has been removed.");
+				Cc.sendS(player, Cc.GREEN, "Removed all mini-regen settings in group '" + groupName + "'.");
+				return true;
 			}
-		} else {
-			Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen <add|remove> [interval] [alias] [distribution] [group] OR /miniregen remove [alias] OR /miniregen remove group [groupName]");
+			if (args[1].equalsIgnoreCase("chunk")) {
+				if (args.length < 3) {
+					Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen remove chunk <alias>");
+					return true;
+				}
+				String alias = args[2];
+				miniRegenManager.removeMiniRegen(alias);
+				Cc.sendS(player, Cc.GREEN, "Removed mini-regen chunk with alias '" + alias + "'.");
+				return true;
+			}
+			String alias = args[1];
+			miniRegenManager.removeMiniRegen(alias);
+			Cc.sendS(player, Cc.GREEN, "Removed mini-regen chunk with alias '" + alias + "'.");
+			return true;
 		}
+		Cc.sendS(player, Cc.YELLOW, "Usage: /miniregen <add|remove> ...");
 		return true;
 	}
 }
